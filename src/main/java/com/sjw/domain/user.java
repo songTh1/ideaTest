a5a6280ce71194021a0beb1ca84fa7c8482bacb3
songTh1 <642158938@qq.com>
2018-12-09 22:13:21 +0800

    一端提交,

--- src/main/java/com/sjw/domain/user.java
+++ src/main/java/com/sjw/domain/user.java
@@ -3,17 +3,15 @@ package com.sjw.domain;
 public class user {
     private  String name;
     private  int age;
+    private  String address;
 
-    public user(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
 
     @Override
     public String toString() {
         return "user{" +
                 "name='" + name + '\'' +
                 ", age=" + age +
+                ", address='" + address + '\'' +
                 '}';
     }
 }
